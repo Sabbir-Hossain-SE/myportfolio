667a72f636e574dcae7442990494ffbd3874bea7
Sabbir Hossain <sabbir.hossain133045@gmail.com>
2021-02-18 08:10:48 +0600

    setup for optimize image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dev|Sabbir`,
+    description: `It is portfolio site to decribe me in details.`,
+    author: `Sabbir`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
